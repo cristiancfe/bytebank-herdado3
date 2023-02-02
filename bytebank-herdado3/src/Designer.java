8900253aae1124ae336cab941f43ca075be2acfb
cristiancfe <cristiancfe@gmail.com>
2023-02-02 13:55:51 -0300

    Finalização

---
+++ bytebank-herdado3/src/Designer.java
@@ -0,0 +1,9 @@
+//Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class Designer extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o metodo de bonificacao do Designer");
+		return 200;
+	}	
+
+}
---
+++ bytebank-herdado3/src/EditorVideo.java
@@ -0,0 +1,9 @@
+//Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class EditorVideo extends Funcionario {
+
+	public double getBonificacao() {
+		System.out.println("Chamando o metodo de bonificacao do Editor de video");
+		return  150;
+	}
+
+}
